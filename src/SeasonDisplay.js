bc0cca560676531cf2daa1e76575d8e408d2df85
Dmytro Prytula <prituladima@gmail.com>
2020-03-16 02:13:24 +0200

    classes rerender example

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SeasonDisplay = () => {
+    return (
+        <div>
+            Season display
+        </div>
+    );
+};
+
+export default SeasonDisplay;
+
+
+//
